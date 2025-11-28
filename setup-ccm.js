d5d18c505a0f3f1e02b3ab87a7e893dd4f6489b0
aeryli <72891343+aeryli@users.noreply.github.com>
2025-11-27 18:47:56 -0800

    fix broken

--- setup-ccm.js
+++ setup-ccm.js
@@ -1,6 +1,8 @@
-Game.lumpsTotal = 1;
-Game.toSave=true;
-location.reload();
+if (Game.lumps) != 0; {
+    Game.lumpsTotal = 1;
+    Game.toSave=true;
+    location.reload();
+};
 document.getElementById('support').remove();
 document.getElementById('smallSupport').remove();
 Game.priceIncrease = 1;
